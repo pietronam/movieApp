ea5d8aa4209ba3408ae015cfda1ee7cd7e655525
nam <pietronam.cavallanti@gmail.com>
2024-03-07 15:06:28 +0100

    Added .gitignore and .env

--- js/api/config.js
+++ js/api/config.js
@@ -5,6 +5,6 @@ export const OPTIONS = {
     headers: {
         "Content-Type": "application/json",
         Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2RkZmNhMjlkNzNhNmEzYTRiYjRkNWYwYzc3MGZiZiIsInN1YiI6IjY1ZTg2YzdlZWE0MjYzMDE3YjIyOGRmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbRmoUz6V4AsxTymqbiS-uORy-HBk8uRXoHXZDA3v3o",
+            "Bearer ",
     },
 };
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-import { getTrending, getTrendingMovies, getTrendingPeople, getTrendingSeries } from "./api/trendingGetters.js";
+import { getTrending, getTrendingMovies, getTrendingPeople, getTrendingSeries  } from "./api/trendingGetters.js";
 import { getSearchPeople } from "./api/searchGetters.js";
-import { BASE_URL } from "./api/config.js";
+import {BASE_URL} from "./api/config.js";
 
 let person_name = "Clooney";
 
