5802fe9be7165d18c510292a20d600678d52918e
nam <pietronam.cavallanti@gmail.com>
2024-03-25 10:58:05 +0100

    getTrending è ora unica ed indipendente dalla categoria, bisogna dare in input l'url.

--- js/api/trendingGetters.js
+++ js/api/trendingGetters.js
@@ -4,41 +4,9 @@ import { OPTIONS, BASE_URL } from "./config.js";
  * @returns media di tendenza
  */
 
-export const getTrending = async () => {
-    const response = await fetch(BASE_URL + "trending/all/day", OPTIONS);
+export const getTrending = async (trendingMediaUrl) => {
+    const response = await fetch(BASE_URL + `${trendingMediaUrl}`, OPTIONS);
     const data = await response.json();
 
     return data;
 };
-
-/**getTrendingMovies
- * @returns film di tendenza
- */
-
-export const getTrendingMovies = async () => {
-    const response = await fetch(BASE_URL + "trending/movie/day", OPTIONS);
-    const data = await response.json();
-    
-    return data;
-}
-/**getTrendingPeople
- * @returns persone di tendenza
- */
-
-export const getTrendingPeople = async () => {
-    const response = await fetch(BASE_URL + "trending/person/day", OPTIONS);
-    const data = await response.json();
-
-    return data;
-}
-
-/**getTrendingSeries
- * @returns series di tendenza
- */
-
-export const getTrendingSeries = async () => {
-    const response = await fetch(BASE_URL + "trending/tv/day", OPTIONS);
-    const data = await response.json();
-
-    return data;
-}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-export { getTrending, getTrendingMovies, getTrendingSeries, getTrendingPeople } from "./api/trendingGetters.js";
+export { getTrending } from "./api/trendingGetters.js";
 export { fixTitle, addParagraph } from "./utils/dom.js";
 export { createTrendingMoviesList, createTrendingList } from "./dom_manipulator/createList.js"
 export { createCards } from "./dom_manipulator/cardFiller.js";
